89dda540943102906ec40ebd8b172421c1edb6f0
manuel <code@leunam.art>
2023-10-10 21:18:30 +0200

    [refactor] removed old unneeded code and added orchid logic

--- app/Providers/LaraviaServiceProvider.php
+++ app/Providers/LaraviaServiceProvider.php
@@ -2,37 +2,53 @@
 
 namespace Laravia\Heart\App\Providers;
 
-use Illuminate\Support\Facades\App;
 use Laravia\Heart\App\Laravia;
+use Orchid\Platform\Dashboard;
+use Orchid\Screen\Actions\Menu;
 
 class LaraviaServiceProvider extends ServiceProvider
 {
     protected $name = 'heart';
 
-    public function register()
+    public function menu(): array
     {
+        //will be refactored with dynamic menu items
+        return [
+            Menu::make(__('Dashboard'))
+                ->icon('bs.heart')
+                ->title(__('System'))
+                ->route('laravia.heart')
+                ->divider(),
+
+
+            //default orchid menu items
+            Menu::make(__('Users'))
+                ->icon('bs.people')
+                ->route('platform.systems.users')
+                ->permission('platform.systems.users')
+                ->title(__('Access Controls')),
+            Menu::make(__('Roles'))
+                ->icon('bs.shield')
+                ->route('platform.systems.roles')
+                ->permission('platform.systems.roles')
+                ->divider(),
+        ];
     }
 
-    public function setMacros()
+    public function boot(Dashboard $dashboard): void
     {
-    }
+        parent::boot($dashboard);
+        $this->defaultBootMethod();
 
-    public function boot()
-    {
-        $this->setMacros();
-
-        $this->loadViewsFrom(Laravia::path()->get($this->name) . '/resources/views', $this->laravia);
-        $this->loadTranslationsFrom(Laravia::path()->get($this->name) . '/lang', $this->laravia);
-        $this->loadMigrationsFrom(Laravia::path()->get($this->name) . '/database/migrations');
-        $this->loadSeedsFrom(Laravia::path()->get($this->name) . '/database/seeders');
-
-        App::booted(function () {
-            $path = Laravia::path()->get($this->name) . '/routes/web.php';
-            $this->loadRoutesFrom($path);
-        });
+        $this->overwriteOrchidSettings();
 
         foreach (Laravia::commands('heart') as $command) {
             $this->commands($command);
         }
+    }
+
+    public function overwriteOrchidSettings(): void
+    {
+        $this->app['config']->set('platform.index', 'laravia.heart');
     }
 }
