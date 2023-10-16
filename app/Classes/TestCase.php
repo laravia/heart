dbf4c63d024d5422d271cda36f4bc69e7d95ddad
manuel <code@leunam.art>
2023-10-16 21:51:13 +0200

    [feat] added actAsAdmin and actAsUser (basic logic)

--- app/Classes/TestCase.php
+++ app/Classes/TestCase.php
@@ -3,6 +3,8 @@
 namespace Laravia\Heart\App\Classes;
 
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Hash;
+use Orchid\Platform\Models\User;
 use ReflectionClass;
 use Tests\TestCase as LaravelTestCase;
 
@@ -34,4 +36,51 @@ class TestCase extends LaravelTestCase
         $reflectionClass = new ReflectionClass($classNameWithNamespace);
         $this->assertTrue($reflectionClass->hasMethod($methodName));
     }
+
+    public function actAsAdmin(){
+        return $this->createAndActAsUser("admin");
+    }
+    public function actAsUser(){
+        return $this->createAndActAsUser("user");
+    }
+
+    public function createAndActAsUser($type = "admin")
+    {
+
+        $roles = [
+            'admin'  => [
+                'platform.index'                      => 1,
+                'platform.systems'                    => 1,
+                'platform.systems.index'              => 1,
+                'platform.systems.roles'              => 1,
+                'platform.systems.settings'           => 1,
+                'platform.systems.users'              => 1,
+                'platform.systems.comment'            => 1,
+                'platform.systems.attachment'         => 1,
+                'platform.systems.media'              => 1,
+            ],
+            'user'   => [
+                'platform.index'                       => 1,
+                'platform.systems'                     => 1,
+                'platform.systems.roles'               => 0,
+                'platform.systems.settings'            => 1,
+                'platform.systems.users'               => 0,
+                'platform.systems.menu'                => 0,
+                'platform.systems.attachment'          => 1,
+                'platform.systems.media'               => 1,
+            ],
+        ];
+
+        $user = User::create([
+            'name'           => $this->faker->firstName,
+            'email'          => $this->faker->unique()->safeEmail,
+            'password'       => Hash::make('password'),
+            'remember_token' => $this->faker->word,
+            'permissions'    => $roles[$type],
+        ]);
+
+        $this->actingAs($user);
+
+        return $user;
+   }
 }
