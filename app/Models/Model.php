269f98ce3208738488da950eca68701e85302680
manuel <code@leunam.art>
2023-10-17 19:06:10 +0200

    [feat] removed default setter and getter (from old core)

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -2,7 +2,6 @@
 
 namespace Laravia\Heart\App\Models;
 
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model as LaravelDefaultModel;
 
 class Model extends LaravelDefaultModel
@@ -13,24 +12,5 @@ class Model extends LaravelDefaultModel
         return $this->dateTimeFormat;
     }
 
-    protected function createdAt(): Attribute
-    {
-        return Attribute::make(
-            get: fn ($value) => ($value ? date($this->dateTimeFormat, strtotime($value)) : null),
-        );
-    }
 
-    protected function updatedAt(): Attribute
-    {
-        return Attribute::make(
-            get: fn ($value) => ($value ? date($this->dateTimeFormat, strtotime($value)) : null),
-        );
-    }
-
-    protected function active(): Attribute
-    {
-        return Attribute::make(
-            set: fn ($value) => ($value ? 1 : null),
-        );
-    }
 }
