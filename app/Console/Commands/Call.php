8ef4b775b77d6cc3a6cfda877d4f1a7fbb5a90df
manuel <code@leunam.art>
2023-10-17 19:08:30 +0200

    [refactor] refactored call artisan to system (for more options)

--- app/Console/Commands/Call.php
+++ app/Console/Commands/Call.php
@@ -17,8 +17,8 @@ class Call extends Command
         foreach (Laravia::config('*.call') as $install) {
             if (is_array($install)) {
                 foreach ($install as $installcode) {
-                    $this->line('Artisan::call(' . $installcode.')');
-                    $this->call($installcode);
+                    $this->line('system("' . $installcode.'")');
+                    system($installcode);
                 }
             }
         }
