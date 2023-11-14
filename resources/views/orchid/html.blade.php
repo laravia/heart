0be05478d70749a3d98dc02a961a256cede827ac
manuel <code@leunam.art>
2023-11-14 12:33:55 +0100

    [feat] added simple empty html for orchid

---
+++ resources/views/orchid/html.blade.php
@@ -0,0 +1,9 @@
+<div class=" d-none d-md-block  layout d-md-flex align-items-center">
+    <div class="bg-white ">
+        <small class="text-muted d-block mb-1">{!! @$title !!}</small>
+        <p class="{!! @$style !!} text-black fw-light mt-auto">
+            {!! @$content !!}
+        </p>
+    </div>
+</div>
+
