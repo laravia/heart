bf97ff62a6f0a4a91650d8cd0e4d4aa7650e4622
manuel <code@leunam.art>
2023-11-28 16:10:48 +0100

    [refactor] excluded soundcloud and mixcloud

--- app/Classes/Embed.php
+++ app/Classes/Embed.php
@@ -14,17 +14,7 @@ class Embed
 
     public function youtube()
     {
-        return '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $this->code . '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-    }
-
-    public function soundcloud()
-    {
-        return '<iframe width="560" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' . $this->code . '&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>';
-    }
-
-    public function mixcloud()
-    {
-        return '<iframe width="560" height="120" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&feed=' . $this->code . '" frameborder="0" ></iframe>';
+        return '<iframe width="100%" height="315" src="https://www.youtube.com/embed/' . $this->code . '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
     }
 
     public function icon()
